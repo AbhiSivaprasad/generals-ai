53d20bce774b398550376cf930ef63bcc539b2d5
Fatih Celikbas <fatih.celikbas@yale.edu>
2019-05-04 19:25:44 -0400

    Update deep_general.py

--- src/players/deep_general.py
+++ src/players/deep_general.py
@@ -9,22 +9,31 @@ class DeepGeneral:
 
     def move(self, board):
         
-        
-        
+        # First get the legal moves
         moves = list(board.legal_moves)
+        
         assert(len(moves) > 0)
+        
+        # e-greedy approach
         if random.random() < self._eps:
             return random.choice(moves)
         else:
+            
+            # get the predicted action
             action = np.argmax(self._model.predict_one(state, self._sess))
-            
-            y = action // (board.cols * 4)
-            x = (action - (y * board.cols * 4)) // 4
-            dr = action - ((y * board.cols * 4) + x * 4)
-            
-            move = Move(x,y, x + DIRECTIONS[dr][0], y + DIRECTIONS[dr][1])
+           
+            move = self.convert_action(action)
             
             if move in moves:
                 return move
             else:
                 return random.choice(moves)
+    
+    def convert_action(action):
+        y = action // (board.cols * 4)
+        x = (action - (y * board.cols * 4)) // 4
+        dr = action - ((y * board.cols * 4) + x * 4)
+        
+        move = Move(x,y, x + DIRECTIONS[dr][0], y + DIRECTIONS[dr][1])
+        
+        return move
