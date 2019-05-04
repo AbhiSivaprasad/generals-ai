ec7de6709bbb95130b39e8a8e8282b6b0c626c84
Fatih Celikbas <fatih.celikbas@yale.edu>
2019-05-03 22:11:56 -0400

    Update deep_general.py

--- src/players/deep_general.py
+++ src/players/deep_general.py
@@ -6,10 +6,20 @@ class DeepGeneral:
         self._eps = eps
 
     def move(self, board):
+        
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        
         moves = list(board.legal_moves)
         assert(len(moves) > 0)
         if random.random() < self._eps:
             return random.choice(moves)
         else:
-            # Have to fix this logic to properly interpret output of the model
-            return np.argmax(self._model.predict_one(state, self._sess))
+            action = np.argmax(self._model.predict_one(state, self._sess))
+            
+            y = action // (self.board_width * 4)
+            x = (action - (y * self.board_width * 4)) // 4
+            dr = action - ((y * self.board_width * 4) + x * 4)
+            
+            move = Move(x,y, x + directions[dr][0], y + directions[dr][1])
+    
+            return move
