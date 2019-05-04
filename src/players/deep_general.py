25a70bf2d56903a6ec13673c5d35e966c551b049
Fatih Celikbas <fatih.celikbas@yale.edu>
2019-05-04 18:55:51 -0400

    Update deep_general.py

--- src/players/deep_general.py
+++ src/players/deep_general.py
@@ -1,5 +1,7 @@
 import random
 import tensorflow as tf
+from src.graphics.constants import *
+
 
 class DeepGeneral:
     def __init__(self, model, eps=0):
@@ -7,7 +9,7 @@ class DeepGeneral:
 
     def move(self, board):
         
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        
         
         moves = list(board.legal_moves)
         assert(len(moves) > 0)
@@ -20,7 +22,7 @@ class DeepGeneral:
             x = (action - (y * board.cols * 4)) // 4
             dr = action - ((y * board.cols * 4) + x * 4)
             
-            move = Move(x,y, x + directions[dr][0], y + directions[dr][1])
+            move = Move(x,y, x + DIRECTIONS[dr][0], y + DIRECTIONS[dr][1])
             
             if move in moves:
                 return move
