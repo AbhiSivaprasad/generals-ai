6588ef263717f7ce9a49995e392d1cde578a0942
Alexi Christakis <alexi.christakis@gmail.com>
2019-05-06 23:06:39 -0400

    board generated, needs legal moves

--- src/graphics/test.py
+++ src/graphics/test.py
@@ -3,12 +3,14 @@ import sys
 sys.path.insert(0, '../../')
 
 from src.board_generator import BoardGenerator
-from src.game_master import GameMaster
+# from src.game_master import GameMaster
+from src.graphics.tile import Tile
 from src.graphics.board import Board
 from src.logger import Logger
 from src.players.random_player import RandomPlayer
-from src.players.deep_general import DeepGeneral
+# from src.players.deep_general import DeepGeneral
 from src.graphics.generals import Generals
+from src.graphics.constants import *
 
 for i in range(1):
 
@@ -19,7 +21,8 @@ for i in range(1):
 
     logger = Logger(num_players=2)
 
-    player = DeepGeneral()
+    # player = DeepGeneral()
+    player = RandomPlayer()
 
     g = Generals(userid='cpsc663_bot_uid', 
                  # username='[Bot] head honcho', # already set
@@ -32,26 +35,45 @@ for i in range(1):
 
 
       print("update:", update)
+      row = update['rows']
+      col = update['cols']
+      pi = update['player_index']
+      gen_y, gen_x = update['generals'][pi]
       army_grid = update['army_grid']
       tile_grid = update['tile_grid']
       generals = update['generals']
       cities = update['cities']
 
+      board = Board(rows=row, cols=col, player_index=None)
 
+      grid = [  # 2D List of Tile Objects
+              [Tile(board, x, y) for x in range(col)]
+              for y in range(row)
+            ]
+
+      print(len(grid), len(grid[0]), row, col)
+
+      # place terrain
+      for x in range(row):
+        for y in range(col):
+          tile = grid[x][y]
+
+          tile.type = tile_grid[x][y]
+          tile.army = army_grid[x][y]
+
+          if x is gen_x and y is gen_y:
+            tile.is_general = True
+            board.generals[0] = tile
+
+          if (x,y) in cities:
+            tile.is_city = True
+            board.cities.append(tile)
+          
+
+      board.set_grid(grid)
 
       startx, starty, destx, desty = player.move(board)
       g.move(startx, starty, destx, desty)
-
-      # get position of your general
-      # pi = update['player_index']
-      # y, x = update['generals'][pi]
-
-      # move units from general to arbitrary square
-      # for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-      #     if (0 <= y+dy < update['rows'] and 0 <= x+dx < update['cols']
-      #             and update['tile_grid'][y+dy][x+dx] != generals.MOUNTAIN):
-      #         g.move(y, x, y+dy, x+dx)
-      #         break
 
 
     # game_master = GameMaster(board, players=[RandomPlayer(), RandomPlayer()], logger=logger)
