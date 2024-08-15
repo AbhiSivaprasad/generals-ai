2bba010292c7583a4e05c04f040f8713aabad802
AbhiSivaprasad <abhi@live.com>
2024-08-15 21:17:56 +0000

    added legal move rate to info dict

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pettingzoo import ParallelEnv
 from gymnasium import spaces
 import numpy as np
@@ -30,7 +31,13 @@ class GeneralsEnvironment(ParallelEnv):
         auxiliary_reward_weight: float = 0.01,
     ):
         self.players = players
-        self.agent_names = [f"player_{i}" for i in range(len(self.players))]
+        self.agent_name_by_player_index = [
+            f"player_{i}" for i in range(len(self.players))
+        ]
+        self.player_index_by_agent_name = {
+            agent_name: i
+            for i, agent_name in enumerate(self.agent_name_by_player_index)
+        }
         self.max_turns = max_turns
         self.board_x_size = board_x_size
         self.board_y_size = board_y_size
@@ -38,11 +45,12 @@ class GeneralsEnvironment(ParallelEnv):
         self.city_probability = city_probability
         self.use_fog_of_war = use_fog_of_war
         self.n_step = 0
+        self.auxiliary_reward_weight = auxiliary_reward_weight
 
         # Define action and observation spaces
         self.action_spaces = {
             agent_name: spaces.Discrete(board_x_size * board_y_size * 4)
-            for agent_name in self.agent_names
+            for agent_name in self.agent_name_by_player_index
         }
         self.observation_spaces = {
             agent_name: spaces.Box(
@@ -55,7 +63,7 @@ class GeneralsEnvironment(ParallelEnv):
                 ),
                 dtype=np.int32,
             )
-            for agent_name in self.agent_names
+            for agent_name in self.agent_name_by_player_index
         }
 
     def reset(self, seed=None, options=None, logger: Logger = None):
@@ -72,22 +80,30 @@ class GeneralsEnvironment(ParallelEnv):
             logger=logger,
             max_turns=self.max_turns,
         )
-        self.previous_player_scores = {agent_name: 0 for agent_name in self.agent_names}
+        self.previous_player_scores = {
+            agent_name: 0 for agent_name in self.agent_name_by_player_index
+        }
 
         # reset player states
         for player in self.players:
             player.reset()
 
         observations = self._get_observations()
-        infos = {agent_name: {} for agent_name in self.agent_names}
+        infos = {agent_name: {} for agent_name in self.agent_name_by_player_index}
         return observations, infos
 
     def step(self, actions):
         # Convert actions to the format expected by game_master
         game_actions = [
             convert_action_index_to_action(actions[agent_name], self.board_x_size)
-            for agent_name in self.agent_names
+            for agent_name in self.agent_name_by_player_index
         ]
+        are_game_actions_legal = {
+            agent_name: self.game_master.board.is_action_valid(
+                game_actions[player_index], player_index
+            )
+            for player_index, agent_name in enumerate(self.agent_name_by_player_index)
+        }
 
         # Execute one tick of the game
         self.game_master.step(game_actions)
@@ -97,7 +113,7 @@ class GeneralsEnvironment(ParallelEnv):
         rewards = self._get_rewards()
         terminations = self._get_terminations()
         truncations = self._get_truncations()
-        infos = {agent_name: {} for agent_name in self.agent_names}
+        infos = self._get_infos(are_game_actions_legal)
         return observations, rewards, terminations, truncations, infos
 
     def _get_observations(self):
@@ -107,7 +123,7 @@ class GeneralsEnvironment(ParallelEnv):
                 len(self.players),
                 fog_of_war=self.use_fog_of_war,
             )[i]
-            for i, agent_name in enumerate(self.agent_names)
+            for i, agent_name in enumerate(self.agent_name_by_player_index)
         }
 
     def _get_rewards(self):
@@ -116,12 +132,12 @@ class GeneralsEnvironment(ParallelEnv):
         # final reward is component 2 + 0.01 * component 1
         player_scores = {
             agent_name: self.game_master.board.get_player_score(i)
-            for i, agent_name in enumerate(self.agent_names)
+            for i, agent_name in enumerate(self.agent_name_by_player_index)
         }
         player_score_changes = {
             agent_name: player_scores[agent_name]
             - self.previous_player_scores[agent_name]
-            for agent_name in self.agent_names
+            for agent_name in self.agent_name_by_player_index
         }
         total_player_score_changes = sum(player_score_changes.values())
         auxiliary_rewards = {
@@ -133,7 +149,7 @@ class GeneralsEnvironment(ParallelEnv):
         total_rewards = {
             agent_name: main_rewards[agent_name]
             + self.auxiliary_reward_weight * auxiliary_rewards[agent_name]
-            for agent_name in self.agent_names
+            for agent_name in self.agent_name_by_player_index
         }
         self.previous_player_scores = player_scores
         return total_rewards
@@ -141,7 +157,7 @@ class GeneralsEnvironment(ParallelEnv):
     def _get_main_rewards(self):
         winning_player_index = self.game_master.board.terminal_status()
         main_rewards = {}
-        for i, agent_name in enumerate(self.agent_names):
+        for i, agent_name in enumerate(self.agent_name_by_player_index):
             if winning_player_index == -1:
                 main_rewards[agent_name] = 0
             else:
@@ -150,11 +166,18 @@ class GeneralsEnvironment(ParallelEnv):
 
     def _get_terminations(self):
         game_over = self.game_master.board.terminal_status() != -1
-        return {agent_name: game_over for agent_name in self.agent_names}
+        return {agent_name: game_over for agent_name in self.agent_name_by_player_index}
 
     def _get_truncations(self):
         truncated = self.n_step >= self.max_turns
-        return {agent_name: truncated for agent_name in self.agent_names}
+        return {agent_name: truncated for agent_name in self.agent_name_by_player_index}
+
+    def _get_infos(self, **kwargs):
+        infos = defaultdict(dict)
+        for info_type, info_dict in kwargs.items():
+            for agent_name, info in info_dict.items():
+                infos[agent_name][info_type] = info
+        return infos
 
     def render(self):
         pass
