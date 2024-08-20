40a5a8c399e1273ec47f6c1fb6aa5b104f533789
Dhruv Shah <dhruv.shah@gmail.com>
2024-08-19 20:56:49 -0700

    fixed the oopsie

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -88,8 +88,10 @@ class GeneralsEnvironment(ParallelEnv):
         return observations, infos
 
     def step(self, actions):
+        player_dict_to_list = lambda player_dict: [player_dict[agent_index] for agent_index in range(len(self.agents))]
+        
         # when serializing observations, serialize them channels-last instead of channels-first i.e. (C, H, W) -> (H, W, C)
-        self.game_master.logger.log_info("obs_tensor", [a.transpose(1, 2, 0).tolist() for a in player_dict_to_list(observations)], self.n_step - 1)
+        self.game_master.logger.log_info("obs_tensor", [a.transpose(1, 2, 0).tolist() for a in player_dict_to_list(self._get_observations())], self.n_step)
         
         # Convert actions to the format expected by game_master
         game_actions = [
@@ -107,7 +109,6 @@ class GeneralsEnvironment(ParallelEnv):
         truncations = self._get_truncations()
         infos = self._get_infos(game_actions)
         
-        player_dict_to_list = lambda player_dict: [player_dict[agent_index] for agent_index in range(len(self.agents))]
         
         
         self.game_master.logger.log_info("rewards", player_dict_to_list(rewards), self.n_step - 1)
