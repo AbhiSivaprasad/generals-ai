aafd3e6818142e5c7b8e43b4b03937d2555c550c
Dhrvu <dhruv.shah@gmail.com>
2024-08-29 13:11:34 +0000

    minor bugfix

--- src/agents/random_agent.py
+++ src/agents/random_agent.py
@@ -14,7 +14,7 @@ class RandomAgent(Agent):
         for i in range(board.num_rows):
             for j in range(board.num_cols):
                 for direction in Direction:
-                    action = Action(startx=i, starty=j, direction=direction)
+                    action = Action(startx=j, starty=i, direction=direction)
                     if board.is_action_valid(action, self.player_index):
                         valid_actions.append(action)
 
